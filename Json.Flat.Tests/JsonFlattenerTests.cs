c19d2d7013384b54314063dae9c2589593cf9449
Peter Goostree <pgoostree@cylance.com>
2019-10-25 15:53:24 -0700

    add test to write flattened name/value pairs to the console

--- Json.Flat.Tests/JsonFlattenerTests.cs
+++ Json.Flat.Tests/JsonFlattenerTests.cs
@@ -1,13 +1,17 @@
 using System.IO;
 using Newtonsoft.Json.Linq;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Json.Flat.Tests
 {
     public class JsonFlattenerTests
     {
-        public JsonFlattenerTests()
+        private ITestOutputHelper _outputHelper;
+
+        public JsonFlattenerTests(ITestOutputHelper outputHelper)
         {
+            _outputHelper = outputHelper;
         }
         
         [Fact(DisplayName = "When Json object is flattened it should contain expected keys")]
@@ -29,5 +33,19 @@ namespace Json.Flat.Tests
             Assert.True(flattened.ContainsKey("address.phone[1].home"));
             Assert.True(flattened.ContainsKey("address.phone[1].work"));
         }
+        
+        [Fact(DisplayName = "Flatten and write the name/value pairs to the console")]
+        public void Fact2()
+        {
+            var data = JObject.Parse(File.ReadAllText(@"jsonStructure1.json"));
+
+            var flattened = JsonFlattener.Flatten(data);
+
+            foreach (var key in flattened.Keys)
+            {
+                _outputHelper.WriteLine($"{key}, {flattened[key]}");
+            }
+        }
+
     }
 }
